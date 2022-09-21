8d2ded2ad802084db8f332535cc6c09dfea8c61e
Patel <patrickngare@gmail.com>
2022-09-21 16:29:23 +0300

    added lombok

--- src/main/java/com/patel/tacocloud/Indegredient.java
+++ src/main/java/com/patel/tacocloud/Indegredient.java
@@ -1,15 +1,13 @@
 package com.patel.tacocloud;
 
+import lombok.Data;
+
+@Data
 public class Indegredient {
     private final String id;
     private final String name;
     private final Type type;
 
-    public Indegredient(String id, String name, Type type) {
-        this.id = id;
-        this.name = name;
-        this.type = type;
-    }
 
     public enum Type{
 
